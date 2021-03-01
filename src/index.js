0b4b799150e565e6392db3fb9156c8e2b1f53376
MajestikButter <75375633+MajestikButter@users.noreply.github.com>
2021-03-01 00:16:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ const { Client } = require('discord.js');
 const bot = new Client();
 
 bot.login(process.env.TOKEN);
+bot.on('ready', () => {
+    console.log(`-------------\n[Logged in]\nUsername: ${bot.user.username}\nID: ${bot.user.id}\n-------------`)
+});
