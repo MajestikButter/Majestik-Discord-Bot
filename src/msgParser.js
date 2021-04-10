5532f3482f48bae9e4d72d6182533bf8ccbe38e4
MajestikButter <75375633+MajestikButter@users.noreply.github.com>
2021-04-10 19:44:28 -0400

    Update msgParser.js
    + argsStr Variable

--- src/msgParser.js
+++ src/msgParser.js
@@ -1,10 +1,10 @@
 require('discord.js');
 
 function parse(sending = '', msg, args = [], argsStr = '') {
-    parsed = sending.
+  parsed = sending.
     replace('${author}', `${msg.author}`).
     replace('${author.username}', `${msg.author.username}`).
-    replace('${author.avatar}', `${msg.author.avatarURL({dynamic: true})}`).
+    replace('${author.avatar}', `${msg.author.avatarURL({ dynamic: true })}`).
     replace('${author.id}', `${msg.author.id}`).
     replace('${channel}', `${msg.channel}`).
     replace('${channel.id}', `${msg.channel.id}`).
@@ -12,22 +12,26 @@ function parse(sending = '', msg, args = [], argsStr = '') {
     replace('${message.id}', `${msg.id}`).
     replace('${guild}', `${msg.guild}`).
     replace('${guild.id}', `${msg.guild.id}`);
-    
-    let splitStr = sending.trim().split(' ');
-    for (let i in splitStr) {
-      entry = splitStr[i];
-      if (!entry.includes('${')) continue;
-      
-      if (args) {
-        if (entry.includes('arg.')) {
-          index = parseInt(entry.replace(/\D/g, ''));
-          entry = `${args[index]}`
-  
-          parsed = parsed.replace(`\${arg.${index}}`, entry);
-        }
+
+  if (args) {
+    parsed.replace('${argsStr}', argsStr);
+  }
+
+  let splitStr = sending.trim().split(' ');
+  for (let i in splitStr) {
+    entry = splitStr[i];
+    if (!entry.includes('${')) continue;
+
+    if (args) {
+      if (entry.includes('arg.')) {
+        index = parseInt(entry.replace(/\D/g, ''));
+        entry = `${args[index]}`
+
+        parsed = parsed.replace(`\${arg.${index}}`, entry);
       }
     }
+  }
 
-    return parsed.replace('$\{', '${');
+  return parsed.replace('$\{', '${');
 };
 module.exports = { parse };
